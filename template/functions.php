5f82c0d15b505a1beca26b6b7ad60f959c84c50d
Drew Baker <drewrb@gmail.com>
2022-04-01 19:58:52 -0700

    Update functions.php

--- template/functions.php
+++ template/functions.php
@@ -18,11 +18,18 @@ add_action("init", "custom_wordpress_setup");
 function custom_theme_setup()
 {
     // Enable post thumbnail support
-    add_theme_support("post-thumbnails");
-    //set_post_thumbnail_size( 600, 400, true ); // Normal post thumbnails
-    //add_image_size( 'banner-thumb', 566, 250, true ); // Small thumbnail size
-    add_image_size("fullscreen", 1920, 0, true);
-    add_image_size("social-preview", 600, 315, true); // Square thumbnail used by sharethis and facebook
+	add_theme_support( 'post-thumbnails' );
+	set_post_thumbnail_size( 960, 540, false );
+	add_image_size( 'social-preview', 1200, 630, true ); // Square thumbnail used by sharethis and facebook
+
+	// You may want to change these, but these defaults cover most use cases
+	add_image_size( 'small-preview', 375, 0, false );
+	add_image_size( 'medium-preview', 960, 0, false );
+	add_image_size( 'large-preview', 1280, 0, false );
+	add_image_size( 'fullscreen-small', 1920, 0, false );
+	add_image_size( 'fullscreen', 2560, 0, false );
+	add_image_size( 'fullscreen-large', 3840, 0, false );
+	add_image_size( 'fullscreen-xlarge', 6016, 0, false );
 
     // Turn on menus
     add_theme_support("menus");
